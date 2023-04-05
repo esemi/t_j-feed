6f2d685133409b5e3a1bb3cf52876d914c61cb61
Simon <spam@esemi.ru>
2023-04-05 19:34:51 +0200

    :recycle: repair tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,7 @@
-from typing import Any
-
 import pytest
 from starlette.testclient import TestClient
 
 from tj_feed.app import webapp
-from tj_feed.storage import create_conn
-
-
-class MockStorage:
-    def __init__(self):
-        self._data = {}
-
-    async def set(self, key, value):
-        self._data[key] = value
-
-    async def get(self, key) -> Any:
-        return self._data.get(key)
-
-    def close(self):
-        pass
-
-    async def wait_closed(self) -> bool:
-        return True
-
-
-@pytest.fixture(autouse=True)
-@pytest.mark.asyncio
-async def mocked_storage(mocker):
-    mocker.patch('aioredis.create_redis_pool', return_value=MockStorage())
-    await create_conn()
 
 
 @pytest.fixture()
